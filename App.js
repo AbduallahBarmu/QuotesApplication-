cff237444904c778dce3472d0f2bb508293f4cc9
unknown <Abduallah.imu2@gmail.com>
2021-12-02 15:45:51 +0300

    last version

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+const API_URL = "https://api.quotable.io/random";
+
+//Function to handle quotes that are coming from API
+function useQuote() {
+  //Define a state with default object values 
+  const [quote, setQuote] = useState({ content: 'Hello world', author: 'AbduAllah' })
+
+  // hook to let us run a function when our component is loaded to the screen
+  useEffect(() => {
+    updateQuote()
+  }, [])
+
+
+  function updateQuote() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => {
+        //If there is an Error Do not set data 
+        if (data.content && data.author) {
+          setQuote({ content: data.content, author: data.author })
+        }
+      })
+  }
+
+  return { quote, updateQuote }
+}
+
+
+
 
 export default function App() {
+  const { quote, updateQuote } = useQuote()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.card}>
+        <Text style={styles.quoteAuthor}>{quote.author} </Text>
+        <Text style={styles.quoteText}> {quote.content}</Text>
+      </View>
+      <View style={styles.quoteButton}>
+        <Button
+          style={styles.quoteButton}
+          onPress={updateQuote}
+          title="New Quote!"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
@@ -17,5 +61,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  card: {
+    backgroundColor: '#f0efeb',
+    paddingBottom: 30,
+    borderRadius: 10,
+    padding: 7,
+  
+  },
+  quoteText: {
+    fontSize: 20,
+    textAlign: "center",
+
+  },
+  quoteAuthor: {
+    marginTop: 15,
+    fontSize: 18,
+    color: "gray",
+    paddingBottom: 20,
+  },
+  quoteButton: {
+    marginTop: 10,
+    borderRadius: 15,
+  
+
+  }
 });
