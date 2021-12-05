1406478d88083428a123e2e937fbe9c328042b4c
unknown <Abduallah.imu2@gmail.com>
2021-12-05 22:49:36 +0300

    update card , button style

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-
 import { QuotesCard } from './components/quoteCard/QuotesCard';
 
 
--- components/quoteCard/QuotesCard.js
+++ components/quoteCard/QuotesCard.js
@@ -1,9 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
-
-import { Card, Button } from 'react-native-elements'
+import { Card } from 'react-native-elements';
 
 
 
@@ -26,7 +25,7 @@ function useQuote() {
             .then(data => {
                 //If there is an Error Do not set data 
                 if (data.content && data.author) {
-                    setQuote({ content: data.content, author: data.author, tags: data.tags, slug: data.slug })
+                    setQuote({ content: data.content, author: data.author, tags: data.tags })
                 }
             })
     }
@@ -40,46 +39,35 @@ function useQuote() {
 
 export const QuotesCard = () => {
     const { quote, updateQuote } = useQuote()
-
+    const btnText = 'Breing me a new quote! ';
+    const title = 'Random Quotes';
 
     return (
-
         <View>
-            <View style={styles.header}>
-
-            </View>
-
-            <Card containerStyle={{
-                marginTop: 60,
-                backgroundColor: '#8d99ae',
-                padding: 7,
-
-            }} >
+            <Card
+                containerStyle={{
+                    width: 400,
+                    height: 300,
+                    marginTop: 60,
+                    backgroundColor: '#8d99ae',
+                }}>
 
                 <Card.Title >
-                    <Text style={styles.title}> Random Quotes </Text>
+                    <Text style={styles.title}> {title} </Text>
                 </Card.Title>
                 <Card.Divider />
-                <Text style={styles.quoteText}> {quote.content}</Text>
+                <Text style={styles.quoteText}> {quote.content} </Text>
                 <Text style={styles.quoteAuthor}>-{quote.author} </Text>
                 <View style={styles.tags}>
-                    <Text style={styles.tagsText}> {quote.tags}</Text>
-                    <Text style={styles.tagsText}> {quote.slug}</Text>
+                    <Text style={styles.tagsText}> {quote.tags} </Text>
                 </View>
             </Card>
-
-
-            <View style={styles.button}>
-                <Button
-                    buttonStyle={{ marginLeft: 0, marginRight: 0, marginBottom: 0, borderRadius: 50, backgroundColor: '#8d99ae' }}
-                    style={styles.btn}
-                    onPress={updateQuote}
-                    title="Breing me a new quote!"
-                />
-
-            </View>
-            <StatusBar style="auto" />
+            <Pressable style={styles.btn} onPress={updateQuote} >
+                <Text style={styles.btnText}>{btnText}</Text>
+            </Pressable>
         </View>
+
+
 
     );
 }
@@ -87,21 +75,28 @@ export const QuotesCard = () => {
 export const styles = StyleSheet.create({
     statusBar: {
         backgroundColor: '#4a4e69',
-
     },
     title: {
-        color: 'white',
+        color: '#fff',
+    },
+    card: {
+        flex: 1,
+        width: 250,
+        // height: 250,
+        marginTop: 60,
+        backgroundColor: '#8d99ae',
+        padding: 7,
+
     },
     quoteText: {
         fontSize: 20,
         textAlign: "center",
-        color: 'white',
-
+        color: '#fff',
     },
     quoteAuthor: {
         marginTop: 15,
         fontSize: 18,
-        color: 'white',
+        color: '#fff',
         paddingBottom: 20,
         textAlign: 'center'
     },
@@ -109,30 +104,38 @@ export const styles = StyleSheet.create({
         display: "flex",
         flexDirection: 'row',
         justifyContent: 'flex-end',
-
-
-
-
     },
     tagsText: {
-        color: 'white',
+        color: '#fff',
         backgroundColor: '#adb5bd',
         padding: 6,
-        marginLeft:10 ,
+        marginLeft: 10,
         borderRadius: 5,
         shadowColor: 'black',
         shadowOffset: { width: -2, height: 4 },
         shadowOpacity: 0.5,
         shadowRadius: 4,
     },
-    button: {
-        marginTop: 10,
-        backgroundColor: '#8d99ae',
-        marginVertical: 8,
-        borderRadius: 10
-
-    },
     btn: {
-        marginVertical: 8,
+
+        width: 300,
+        marginTop: 30,
+        marginLeft: 60,
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: "column",
+        paddingVertical: 15,
+        // justifyContent: 'center',
+        // paddingHorizontal: 10,
+        // borderRadius: 4,
+        // elevation: 2,
+        borderRadius: 100 / 2,
+        backgroundColor: '#8d99ae'
+    },
+    btnText: {
+        color: '#fff',
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
     }
 });
